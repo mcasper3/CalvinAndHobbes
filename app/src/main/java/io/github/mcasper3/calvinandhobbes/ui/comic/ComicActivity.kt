2963ff290f8b44cc0a81b76d0e4c329d669fa042
Mike Casper <Mikecasper395@gmail.com>
2017-04-03 13:01:05 -0500

    date changes

--- app/src/main/java/io/github/mcasper3/calvinandhobbes/ui/comic/ComicActivity.kt
+++ app/src/main/java/io/github/mcasper3/calvinandhobbes/ui/comic/ComicActivity.kt
@@ -2,9 +2,11 @@ package io.github.mcasper3.calvinandhobbes.ui.comic
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v4.view.ViewPager
 import android.widget.TextView
 import butterknife.bindView
@@ -16,6 +18,7 @@ class ComicActivity : AppCompatActivity() {
     val mDateFill: TextView by bindView(R.id.comic_date_fill)
     val mViewPager: ViewPager by bindView(R.id.comic_view_pager)
     lateinit var mAdapter: ComicPagerAdapter
+    lateinit var mSharedPrefs: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +29,8 @@ class ComicActivity : AppCompatActivity() {
 
         mDateOutline.typeface = outline
         mDateFill.typeface = normal
+
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
 
         mAdapter = ComicPagerAdapter(supportFragmentManager)
         mViewPager.adapter = mAdapter
@@ -40,10 +45,15 @@ class ComicActivity : AppCompatActivity() {
 
             override fun onPageSelected(position: Int) {
                 updateDate(mAdapter.getDate(position))
+                mSharedPrefs.edit()
+                        .putInt(SHARED_PREF_LAST_COMIC, position)
+                        .apply()
             }
-
         })
 
+        val position = mSharedPrefs.getInt(SHARED_PREF_LAST_COMIC, 0)
+        mViewPager.setCurrentItem(position, false)
+        updateDate(mAdapter.getDate(position))
     }
 
     override fun onDestroy() {
@@ -56,6 +66,8 @@ class ComicActivity : AppCompatActivity() {
     }
 
     companion object {
+        val SHARED_PREF_LAST_COMIC = "sharedPref_lastComic"
+
         fun createIntent(context: Context): Intent {
             return Intent(context, ComicActivity::class.java)
         }
